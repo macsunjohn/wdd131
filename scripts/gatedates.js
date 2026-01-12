7fb3a6c227c4173f2cfa948a67e45020d58c672e
macsunjohn <marksunjohn@gmail.com>
2026-01-12 22:12:23 +0100

    Commit changes

--- scripts/gatedates.js
+++ scripts/gatedates.js
@@ -1,5 +1,4 @@
 document.getElementById("currentyear").textContent =
     new Date().getFullYear();
 
-    // Display last modified date
 document.getElementById("lastModified").textContent = document.lastModified;
