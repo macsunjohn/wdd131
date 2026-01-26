2e3f82ba73a7eaca844ef0619a295ba5f4bb9adf
macsunjohn <marksunjohn@gmail.com>
2026-01-26 12:10:40 +0100

    commit new update

--- scripts/place.js
+++ scripts/place.js
@@ -1,5 +1,5 @@
-const temp = 28; // °C
-const windSpeed = 10; // km/h
+const temp = 10;       // °C
+const windSpeed = 5;  // km/h
 
 function calculateWindChill(t, v) {
   return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
