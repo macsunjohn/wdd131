667eaedbb0d6092aa512b1250686ae92404256f2
macsunjohn <marksunjohn@gmail.com>
2026-01-26 03:26:32 +0100

    commit place

---
+++ scripts/place.js
@@ -0,0 +1,16 @@
+const temp = 28; // °C
+const windSpeed = 10; // km/h
+
+function calculateWindChill(t, v) {
+  return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
+}
+
+let windChill = "N/A";
+
+if (temp <= 10 && windSpeed > 4.8) {
+  windChill = calculateWindChill(temp, windSpeed).toFixed(1) + " °C";
+}
+
+document.getElementById("windchill").textContent = windChill;
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
