5811d37b3b98f59f375b26d7f3574a5ec71aa5cb
Ijeoma Ani <marksunjohn@gmail.com>
2026-02-15 22:59:37 +0100

    created the about page and added content

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -4,3 +4,22 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 // Last modified date
 document.getElementById("lastModified").textContent =
   "Last Modification: " + document.lastModified;
+
+
+  // Togglable Vertical Tabs
+  function openItem(evt, itemName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(itemName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
