8e02e0120605296d4fcbf7aa6806324859a49014
Ijeoma Ani <marksunjohn@gmail.com>
2026-02-15 18:59:54 +0100

    updated the footer

--- project/scripts/sights.js
+++ project/scripts/sights.js
@@ -38,3 +38,11 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+// Current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Last modified date
+document.getElementById("lastModified").textContent =
+  "Last Modification: " + document.lastModified;
